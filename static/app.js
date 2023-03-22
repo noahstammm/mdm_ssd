50a7272030c1a4ab8687d6afaeab3d03b280ca4e
noahstammm <stammnoa@students.zhaw.ch>
2023-03-22 13:32:37 +0100

    delete directory

--- static/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#punctuate-form').submit(function(event) {
-        event.preventDefault();
-        var text = $('#text-input').val();
-        $.post('/punctuate', {text: text}, function(data) {
-            $('#result').html(data);
-        });
-    });
-});
